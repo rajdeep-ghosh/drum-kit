518c9152d81ec24cfd85c3890dd366d74a35afce
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2020-10-11 06:56:50 +0530

    Add background image to the drum buttons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i=0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!!");
+        var tom_1 = new Audio("sounds/tom-1.mp3");
+        tom_1.play();
     });
 }
