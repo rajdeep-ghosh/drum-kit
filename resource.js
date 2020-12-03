859b09ae047f183102085d04b5a24dd5c8c1c85a
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2020-12-03 23:52:11 +0530

    Create resource.js for audio load

---
+++ resource.js
@@ -0,0 +1,20 @@
+var tom_1 = new Audio('sounds/tom-1.mp3');
+tom_1.load();
+
+var tom_2 = new Audio('sounds/tom-2.mp3');
+tom_2.load();
+
+var tom_3 = new Audio('sounds/tom-3.mp3');
+tom_3.load();
+
+var tom_4 = new Audio('sounds/tom-4.mp3');
+tom_4.load();
+
+var snare = new Audio('sounds/snare.mp3');
+snare.load();
+
+var crash = new Audio('sounds/crash.mp3');
+crash.load();
+
+var kick = new Audio('sounds/kick-bass.mp3');
+kick.load();
